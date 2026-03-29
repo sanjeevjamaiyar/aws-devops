2dbe714f8e38c771fa83cbad57fe74cc28ff84df
sanjeevjamaiyar <115970346+sanjeevjamaiyar@users.noreply.github.com>
2026-03-29 23:26:31 +1100

    Update updatebackendnew.sh

--- DevOps-Mega-Project-main/Automations/updatebackendnew.sh
+++ DevOps-Mega-Project-main/Automations/updatebackendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-0c1253b067c3d531b"
+INSTANCE_ID="i-0de0d3973388d01ef"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
